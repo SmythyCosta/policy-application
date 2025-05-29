ae1e1c0c6a717a36478fb6f351843e40656801b6
adam-costa_bradesco <adam.costa@bradesco.com.br>
2025-05-29 09:47:01 -0300

    initial commit

---
+++ PolicyApplication/Controllers/PolicyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PolicyApplication.Models;
+
+namespace PolicyApplication.Controllers
+{
+    [ApiController]
+    [Route("policy")]
+    public class PolicyController : ControllerBase
+    {
+        [HttpGet("{policyNumber}")]
+        public ActionResult<Policy> GetPolicy(string policyNumber)
+        {
+            // Dados mockados
+            if (policyNumber == "000222333")
+            {
+                var policy = new Policy
+                {
+                    PolicyNumber = "000222333",
+                    Value = 15000.50m,
+                    Insured = "João da Silva"
+                };
+                return Ok(policy);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ PolicyApplication/Models/PolicyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PolicyApplication.Models
+{
+    public class Policy
+    {
+        public string PolicyNumber { get; set; }
+        public decimal Value { get; set; }
+        public string Insured { get; set; }
+    }
+}
